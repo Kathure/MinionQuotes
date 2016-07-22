1221e9dc4b371244a26da7555a3e3ab084334a02
Kathure <lizkathure@gmail.com>
2016-07-22 12:14:01 +0300

    added appintro

--- app/src/main/java/com/kathure/minionquotes/IntroActivity.java
+++ app/src/main/java/com/kathure/minionquotes/IntroActivity.java
@@ -1,13 +1,59 @@
 package com.kathure.minionquotes;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class IntroActivity extends AppCompatActivity {
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+
+public class IntroActivity extends AppIntro {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro);
+
+        addSlide(SampleSlide.newInstance(R.layout.intro_layout));
+        addSlide(SampleSlide.newInstance(R.layout.intro_layout2));
+        addSlide(SampleSlide.newInstance(R.layout.fragment_intro));
+        addSlide(SampleSlide.newInstance(R.layout.fragment_intro_content));
+
+        // OPTIONAL METHODS
+        // Override bar/separator color.
+        setBarColor(Color.parseColor("#3F51B5"));
+        setSeparatorColor(Color.parseColor("#2196F3"));
+
+        // Hide Skip/Done button.
+        showSkipButton(false);
+        setProgressButtonEnabled(false);
+
+        // Turn vibration on and set intensity.
+        // NOTE: you will probably need to ask VIBRATE permisssion in Manifest.
+        setVibrate(true);
+        setVibrateIntensity(30);
     }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        // Do something when users tap on Skip button.
+        finish();
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        // Do something when users tap on Done button.
+        finish();
+    }
+
+    @Override
+    public void onSlideChanged(@Nullable Fragment oldFragment, @Nullable Fragment newFragment) {
+        super.onSlideChanged(oldFragment, newFragment);
+        // Do something when the slide changes.
+    }
+
 }
