3640f53c286c8d2b647091027d098e13fe0f1b18
Kathure <lizkathure@gmail.com>
2016-07-23 18:11:07 +0300

    added youtube library api

--- app/src/main/java/com/kathure/minionquotes/MainActivity.java
+++ app/src/main/java/com/kathure/minionquotes/MainActivity.java
@@ -9,5 +9,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        final YouTubeInitializationResult result = YouTubeApiServiceUtil.isYouTubeApiServiceAvailable(this);
+
+        if (result != YouTubeInitializationResult.SUCCESS) {
+            //If there are any issues we can show an error dialog.
+            result.getErrorDialog(this, 0).show();
+        //static {
+            //addItem(new YouTubeVideo("qTSDL94_Y7M", "Best of Minions"));
+            //addItem(new YouTubeVideo("J-uIVtgxHwY", "More on Minions"));
+            //addItem(new YouTubeVideo("T09keZ78QVBs", "Minions Banana"));
+        //}
+        }
 }
